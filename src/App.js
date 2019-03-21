22d3c159be1b37f688822342cb23d902c42fce1c
Alex Su <alexanderytsu@gmail.com>
2019-03-21 19:13:03 +1100

    [api]: created function that gets repos and its tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getUserRepositories } from "./api/repositories";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  async componentDidMount() {
+    const data = await getUserRepositories("alexytsu");
+
+    data.forEach(function(item, index, theArray) {
+      console.log(item);
+      theArray[index].tag = item.topics ? item.topics[0] : "null";
+    });
+
+    this.setState({ data });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+
+        <ul>
+          {this.state.data.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url}>
+                {repo.tag} {repo.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
---
+++ src/api/repositories.js
@@ -0,0 +1,9 @@
+export async function getUserRepositories(username) {
+  let response = await fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: {
+      Accept: "application/vnd.github.mercy-preview+json"
+    }
+  });
+  let data = await response.json();
+  return data;
+}
